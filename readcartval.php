d252871af3d512bed890d4148206393ac7ec3617
ryan-masson-hiscock <98837233+ryan-masson-hiscock@users.noreply.github.com>
2022-03-01 12:01:16 -0500

    Update readcartval.php

--- readcartval.php
+++ readcartval.php
@@ -2,7 +2,7 @@
 <html>
 <?php
     $logic = `gpio read 27`;
-    if($logic == "1")
+    if($logic == '1')
         echo "The motors are currently in brake mode"; 
     else
         echo "The motors are currently in running mode";
