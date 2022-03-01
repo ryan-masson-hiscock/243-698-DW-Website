498019fd25400ba651bc24eea00145120cf52f46
ryan-masson-hiscock <98837233+ryan-masson-hiscock@users.noreply.github.com>
2022-03-01 11:50:38 -0500

    Update readcartval.php

--- readcartval.php
+++ readcartval.php
@@ -2,11 +2,9 @@
 <html>
 <?php
     $logic = `gpio read 27`;
-    if($logic = "1") {
-        echo "The motors are currently stopped";
-    }
-        else {
+    if($logic = "1")
+        echo "The motors are currently stopped"; 
+    else
         echo "The motors are currently running";
-    }
 ?>
 </html>
